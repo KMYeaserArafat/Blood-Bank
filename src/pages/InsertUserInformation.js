810f07c78cab0d43def39257e6fd9f2ba0276488
Arafat <kmyeaserarafat1971@gmail.com>
2024-10-01 20:12:41 +0600

    @All_Dome

--- src/pages/InsertUserInformation.js
+++ src/pages/InsertUserInformation.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './InsertUserInformation.css';
 
 const InsertUserInformation = () => {
+
   return (
-    <div>
-       <h1>Insert User Information</h1>
+    <div id="Insert-User-Info-Body">
+
+      <form>
+         <h1 id="Insert-User-Info-title">Insert User Information</h1>
+         
+         <label>Name : </label>
+         <input type='text' name='name' />
+
+         <label>Area : </label>
+         <textarea name='area' />
+
+         <label>Phone : </label>
+         <input type="text" name='phone'/>
+
+         <label>Email : </label>
+         <input type='text' name='email' />
+
+         <button id='btn-Reset'>Reset</button>
+         <button id='btn-Insert'>Insert</button>
+         <button id='btn-Contact'>Show All Contact</button>
+      </form>
+       
     </div>
   )
 }
